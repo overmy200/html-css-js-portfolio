de62a9e2c946b8e054aff1ab7147db10a13192a2
overmy200 <114469268+overmy200@users.noreply.github.com>
2023-08-24 22:19:10 +0700

    added channel name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toggleMenu(){
-    menu = document.querySelector(".menu-links");
-    icon = document.querySelector(".menu-icon");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
