a448eb58674007cc8d2d782979e32175d293312a
overmy200 <114469268+overmy200@users.noreply.github.com>
2023-08-24 21:15:07 +0700

    Added new files

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    menu = document.querySelector(".menu-links");
+    icon = document.querySelector(".menu-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
